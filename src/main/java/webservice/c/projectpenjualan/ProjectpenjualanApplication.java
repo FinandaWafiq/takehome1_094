0c63cc09223c800b24e53bba21564147f690a7a8
Finanda Wafiq <finnandawafiq@gmail.com>
2022-10-30 19:42:20 +0700

    Membuat file Table Controller

---
+++ src/main/java/webservice/c/projectpenjualan/ProjectpenjualanApplication.java
@@ -0,0 +1,13 @@
+package webservice.c.projectpenjualan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectpenjualanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectpenjualanApplication.class, args);
+	}
+
+}
